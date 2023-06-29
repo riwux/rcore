5079d0317466d388ee0d75f2f8bc3c4fc0f27346
Tom Schwindl <schwindl@posteo.de>
2023-06-29 21:57:14 +0200

    include: add util.h

---
+++ include/util.h
@@ -0,0 +1,27 @@
+/* See LICENSE file for copyright and license details. */
+#ifndef UTIL_H_
+#define UTIL_H_
+
+#include <sys/types.h> /* mode_t ; (s)size_t */
+
+#define TOP_DOWN 0
+#define BOTTOM_UP 1
+
+typedef struct Buf Buf;
+struct Buf {
+	char *data;
+	size_t size;
+};
+
+Buf *buf_create(size_t);
+
+void die(char *);
+void tell(char *);
+
+ssize_t get_line(int, Buf *);
+
+void *emalloc(size_t);
+void *erealloc(void *, size_t);
+
+
+#endif /* UTIL_H_ */
