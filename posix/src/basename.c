f749f9342d57df6eac82a47d232ce7a9782fdd47
Tom Schwindl <schwindl@posteo.de>
2024-04-27 12:30:59 +0200

    posix/basename: use optind as all the other tools

--- posix/src/basename.c
+++ posix/src/basename.c
@@ -25,15 +25,16 @@ main(int argc, char *argv[])
 			break;
 		}
 	}
+	argc -= optind;
+	argv += optind;
 
-	if (argc != 2 && argc != 3)
+	if (argc != 1 && argc != 2)
 		usage();
 
-	base = basename(argv[1]);
-
-	if (argc == 3) {
-		suffix = (base + strlen(base)) - strlen(argv[2]);
-		if (suffix != base && !strcmp(suffix, argv[2]))
+	base = basename(argv[0]);
+	if (argc == 2) {
+		suffix = (base + strlen(base)) - strlen(argv[1]);
+		if (suffix != base && !strcmp(suffix, argv[1]))
 			*suffix = '\0';
 	}
 
