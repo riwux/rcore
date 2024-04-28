e61bb3cb8b0fe535efde1663dffbf8207ee306b2
Tom Schwindl <schwindl@posteo.de>
2024-04-28 17:47:34 +0200

    posix/cmp: move logic out of main
    I think the main function should only contain initialization and/or loop
    functionality. All actual application logic should move out of main and
    into separate funtions. This way it's easier to see and understand what
    actually does the heavy lifting and what's just setup code.

--- posix/src/cmp.c
+++ posix/src/cmp.c
@@ -12,15 +12,51 @@ usage(void)
 	die(2, "usage: cmp [-l|-s] file1 file2");
 }
 
+static bool
+compare_files(char *fn[], FILE *fp[], bool lflag, bool sflag)
+{
+	size_t n  = 1;
+	size_t ln = 1;
+	int c[2]  = {-2, -2};
+
+	for (int i = 0; ; i=!i, n+=!i) {
+		c[i] = fgetc(fp[i]);
+		if (c[i] == EOF) {
+			if (c[!i] != EOF && fgetc(fp[!i]) != EOF)
+				fn[2] = fn[i];
+			break;
+		}
+		if (c[!i] == -2)
+			continue;
+		if (c[i] == '\n' && c[!i] == '\n')
+			++ln;
+		if (c[i] != c[!i]) {
+			if (sflag) {
+				break;
+			} else if (lflag) {
+				printf("%zu %o %o\n", n, c[!i], c[i]);
+			} else {
+				printf("%s %s differ: byte %zu, line %zu\n", \
+				        fn[!i], fn[i], n, ln);
+				break;
+			}
+		}
+		c[i] = c[!i] = -2;
+	}
+	if (fn[2]) {
+		eprintf("cmp: EOF on %s after byte %zu\n", \
+		         fn[2], n-1);
+	}
+
+	return (c[0] != c[1]);
+}
+
 int
 main(int argc, char *argv[])
 {
 	bool lflag = false;
 	bool sflag = false;
 	int opt;
-	size_t n  = 1;
-	size_t ln = 1;
-	int c[2]    = {-2, -2};
 	char *fn[3] = {NULL, NULL, NULL};
 	FILE *fp[2] = {NULL, NULL};
 
@@ -51,37 +87,10 @@ main(int argc, char *argv[])
 		if (!fp[i])
 			die(2, "fopen: '%s':", fn[i]);
 	}
+
 	/* Is the file compared to itself? */
 	if (!strcmp(fn[0], fn[1]))
 		return 0;
 
-	for (int i = 0; ; i=!i, n+=!i) {
-		c[i] = fgetc(fp[i]);
-		if (c[i] == EOF) {
-			if (c[!i] != EOF && fgetc(fp[!i]) != EOF)
-				fn[2] = fn[i];
-			break;
-		}
-		if (c[!i] == -2)
-			continue;
-		if (c[i] == '\n' && c[!i] == '\n')
-			++ln;
-		if (c[i] != c[!i]) {
-			if (sflag) {
-				break;
-			} else if (lflag) {
-				printf("%zu %o %o\n", n, c[!i], c[i]);
-			} else {
-				printf("%s %s differ: byte %zu, line %zu\n", \
-				        fn[!i], fn[i], n, ln);
-				break;
-			}
-		}
-		c[i] = c[!i] = -2;
-	}
-	if (fn[2]) {
-		eprintf("cmp: EOF on %s after byte %zu\n", \
-		         fn[2], n-1);
-	}
-	return (c[0] != c[1]);
+	return compare_files(fn, fp, lflag, sflag);
 }
