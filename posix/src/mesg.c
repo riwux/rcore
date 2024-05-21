7240c208a388b4a86a92e7aa754beb32ce943841
Tom Schwindl <schwindl@posteo.de>
2024-05-21 16:39:00 +0200

    posix: add license header and some minor changes while at it

--- posix/src/mesg.c
+++ posix/src/mesg.c
@@ -1,14 +1,42 @@
-/* See LICENSE file for copyright and license details. */
+/*
+ * Copyright 2023-2024 Tom Schwindl <schwindl@posteo.de>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS
+ * IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * mesg - permit or deny messages from other users to the current terminal
+ * ref: https://pubs.opengroup.org/onlinepubs/9699919799/utilities/mesg.html
+ */
 #include <stdio.h>
-#include <sys/stat.h>
 #include <unistd.h>
+#include <sys/stat.h>
 
 #include "util.h"
 
 static void
 usage(void)
 {
-	die(2, "usage: mesg [y|n]");
+	die(2, "usage: mesg [y | n]");
 }
 
 int
@@ -31,25 +59,21 @@ main(int argc, char *argv[])
 
 	tty = ttyname(STDERR_FILENO);
 	if (!tty)
-		die(2, "mesg: ttyname:");
+		die(2, "mesg: ttyname 'STDERR_FILENO':");
 	if (stat(tty, &st))
-		die(2, "mesg: stat:");
+		die(2, "mesg: stat '%s':", tty);
 
 	switch (argc) {
 	case 0:
-		if (st.st_mode & S_IWGRP)
-			puts("is y");
-		else
-			puts("is n");
+		puts((st.st_mode & S_IWGRP) ? "is y" : "is n");
 		break;
 	case 1:
-		if (**argv == 'y' || **argv == 'Y')
+		if (**argv == 'y')
 			mode = st.st_mode | S_IWGRP;
-		else if (**argv == 'n' || **argv == 'N')
+		else if (**argv == 'n')
 			mode = st.st_mode & ~S_IWGRP;
 		else
 			die(2, "mesg: invalid argument '%c'", **argv);
-
 		if (chmod(tty, mode))
 			die(2, "mesg: chmod:");
 		break;
--- posix/src/mkfifo.c
+++ posix/src/mkfifo.c
@@ -1,4 +1,33 @@
-/* See LICENSE file for copyright and license details. */
+/*
+ * Copyright 2023-2024 Tom Schwindl <schwindl@posteo.de>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS
+ * IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * mkfifo - create named pipes (FIFOs)
+ * ref: https://pubs.opengroup.org/onlinepubs/9699919799/utilities/mkfifo.html
+ */
+
 #include <errno.h>
 #include <stdio.h>
 #include <string.h>
@@ -11,7 +40,7 @@
 static void
 usage(void)
 {
-	die(1, "usage: mkfifo [-m mode] file...");
+	die(1, "usage: mkfifo [-m mode] name...");
 }
 
 int
@@ -19,12 +48,14 @@ main(int argc, char *argv[])
 {
 	int opt;
 	int ret = 0;
-	mode_t mode = S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH;
+	mode_t mode = (S_IRUSR|S_IWUSR|S_IRGRP|S_IWGRP|S_IROTH|S_IWOTH);
 
 	while ((opt = getopt(argc, argv, "m:")) != -1) {
 		switch (opt) {
 		case 'm':
-			mode = x_parsemode(optarg, mode);
+			umask(0);
+			if ((mode = x_parsemode(optarg, mode)) > 0777)
+				die(1, "mkfifo: mode must only contain file permission bits");
 			break;
 		default:
 			usage();
@@ -33,6 +64,9 @@ main(int argc, char *argv[])
 	}
 	argc -= optind;
 	argv += optind;
+
+	if (argc == 0)
+		usage();
 
 	for (; *argv; ++argv) {
 		if (mkfifo(*argv, mode)) {
