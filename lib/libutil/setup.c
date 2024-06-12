3c1996ef686190e6c924a9ecad316f1b442fdbbc
Tom Schwindl <schwindl@posteo.de>
2024-06-12 11:32:04 +0200

    libutil: add missing setup() implementation

---
+++ lib/libutil/setup.c
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023-2024 Tom Schwindl <schwindl@posteo.de>
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ * this list of conditions and the following disclaimer in the documentation
+ * and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS
+ * IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ * ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *
+ * This file contains special functions that are necessary to execute
+ * all utilities in a similiar and consistent way.
+ */
+#include "util.h"
+
+/*
+ * Store the name of the program globally. Thus, functions that are not part
+ * of a specific utility can access the name of it anyway. This makes it
+ * possible to prefix error/warning messages with the name to stay consistent.
+ */
+char const *_prog;
+
+void
+setup(char const *const progname, char *argv[])
+{
+	if (!argv[0])
+		die(1, "A NULL argv[0] was passed to the program");
+	else
+		_prog = progname;
+}
